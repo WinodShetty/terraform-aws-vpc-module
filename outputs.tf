45c9e75f49c73f96bb16343a3fb3dcf9a64eadac
vinod <mechvnod@gmail.com>
2025-12-27 20:41:33 +0530

    vpc

--- outputs.tf
+++ outputs.tf
@@ -22,3 +22,20 @@ output "private_subnet_ids" {
 output "database_subnet_ids" {
   value = aws_subnet.database[*].id
 }
+
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
