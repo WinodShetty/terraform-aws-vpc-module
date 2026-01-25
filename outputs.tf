e134e681c1a0123afd8347bbcf3a17b0d9d24511
vinod <mechvnod@gmail.com>
2026-01-25 18:13:14 +0530

    terraform

--- outputs.tf
+++ outputs.tf
@@ -463,3 +463,11 @@ output "vpc_id" {
 output "public_subnet_ids" {
   value = aws_subnet.public[*].id
 }
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
